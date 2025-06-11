d98d125b2dea8eb3784b518b924816f6acae09e8
martinecvia <martin.coplak@viapont.cz>
2025-06-11 19:32:19 +0200

    First attempt to make Server<>Backup structure

--- pterodactyl/server.py
+++ pterodactyl/server.py
@@ -1,5 +1,23 @@
-import dataclasses
+from dataclasses import dataclass
+from datetime import datetime
 
-@dataclasses
+@dataclass
 class Server:
-    
+    @dataclass
+    class Backup:
+        uuid: str
+        name: str
+        bytes: int
+        created_at: datetime
+        completed_at: datetime | None
+
+        def is_locked(self) -> bool:
+            return False
+        
+    identifier: str | None
+    uuid: str
+    name: str
+    is_suspended: bool
+    is_installing: bool
+
+    backups: list[Backup] = []
